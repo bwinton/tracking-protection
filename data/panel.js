0805dc6e192ea09bbed8417a42224bc734d5442c
Robert Helmer <rhelmer@rhelmer.org>
2016-03-08 11:48:47 -0800

    start wiring up message passing so content panel changes as tabs and loaded URLs change

--- data/panel.js
+++ data/panel.js
@@ -4,3 +4,28 @@ window.addEventListener("click", (event) => {
     self.port.emit("toggle", t.toString());
   }
 }, false);
+
+self.port.on("reset", () => {
+  console.log("reset");
+  document.getElementById("toggle").disabled = true;
+  document.getElementById("report").disabled = true;
+  var textarea = document.getElementById("report-content");
+  textarea.value = "";
+  textarea.disabled = true;
+});
+
+self.port.on("enabled", () => {
+  console.log("enabled");
+  document.getElementById("toggle").value = "Disable and report";
+  document.getElementById("toggle").disabled = false;
+  document.getElementById("report").disabled = false;
+  document.getElementById("report-content").disabled = false;
+});
+
+self.port.on("disabled", () => {
+  console.log("disabled");
+  document.getElementById("toggle").value = "Enable";
+  document.getElementById("toggle").disabled = false;
+  document.getElementById("report").disabled = false;
+  document.getElementById("report-content").disabled = false;
+});
--- index.js
+++ index.js
@@ -37,8 +37,7 @@ let panel = panels.Panel({
   }
 });
 
-panel.port.on("toggle", (addonMessage) => {
-  let activeTab = tabs.activeTab;
+function normalizeUrl(url) {
   // NOTE below is from:
   // https://dxr.mozilla.org/mozilla-central/rev/be593a64d7c6a826260514fe758ef32a6ee580f7/browser/base/content/browser-trackingprotection.js
   // Convert document URI into the format used by
@@ -48,8 +47,36 @@ panel.port.on("toggle", (addonMessage) => {
 
   // FIXME there must be a way to get at the nsIURI in activeTab...
   // what we really want is hostPort
-  let url = activeTab.url.replace(/^http:\/\//, "https://");
-  let normalizedUrl = Services.io.newURI(url, null, null);
+  let hostPort = url.replace(/^http:\/\//, "https://");
+  return Services.io.newURI(hostPort, null, null);
+}
+
+// a new tab has been selected
+tabs.on("activate", () => {
+  console.log("active:", tabs.activeTab.url);
+});
+
+// DOM has loaded but page isn't finished, reset the add-on controls
+tabs.on("ready", (tab) => {
+  panel.port.emit("reset");
+});
+
+// page is loaded from back/forward cache
+// This also fires when regular pages are done loading.
+tabs.on("pageshow", (tab) => {
+  let normalizedUrl = normalizeUrl(tabs.activeTab.url);
+  if (Services.perms.testPermission(normalizedUrl, "trackingprotection")) {
+    console.log("tracking protection *not* already active for:", normalizedUrl.spec);
+    panel.port.emit("disabled");
+  } else {
+    console.log("tracking protection already active for:", normalizedUrl.spec);
+    panel.port.emit("enabled");
+  }
+});
+
+panel.port.on("toggle", (addonMessage) => {
+  let activeTab = tabs.activeTab;
+  let normalizedUrl = normalizeUrl(activeTab.url);
 
   if (Services.perms.testPermission(normalizedUrl, "trackingprotection")) {
     Services.perms.remove(normalizedUrl, "trackingprotection");
