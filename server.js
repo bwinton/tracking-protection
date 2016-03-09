4111e43826895859e86efb8c11390ae19cb65627
Robert Helmer <rhelmer@rhelmer.org>
2016-03-09 15:54:36 -0800

    use strict and es6 features

--- server.js
+++ server.js
@@ -1,32 +1,35 @@
-var PORT = process.env.PORT || 5000;
-var formidable = require('formidable'),
-    http = require('http'),
-    util = require('util');
+"use strict";
+
+let PORT = process.env.PORT || 5000;
+let formidable = require("formidable"),
+    http = require("http"),
+    util = require("util");
 
 http.createServer(function(req, res) {
-  if (req.method.toLowerCase() == 'post') {
+  if (req.method.toLowerCase() == "post") {
     console.log("incoming POST request");
     if (!req.url == "/upload") {
         console.error("invalid URL:", req.url);
         return;
     }
-    var form = new formidable.IncomingForm();
+    let form = new formidable.IncomingForm();
     form.parse(req, function(err, fields, files) {
       if (err) {
         console.error(err.message);
         return;
       }
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.writeHead(200, {'content-type': 'text/plain'});
-      res.write('received upload:\n\n');
-      res.end(util.inspect({fields: fields, files: files}));
-      console.log(util.inspect({fields: fields, files: files}));
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.writeHead(200, {"content-type": "text/plain"});
+      res.write("received upload:\n\n");
+      let json_response = util.inspect({fields: fields, files: files});
+      res.end(json_response);
+      console.log(json_response);
     });
     return;
   } else {
     console.error("invalid request method", req.method);
   }
-  res.writeHead(200, {'content-type': 'text/plain'});
+  res.writeHead(200, {"content-type": "text/plain"});
   res.end();
 }).listen(PORT);
 
