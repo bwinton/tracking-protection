3c881c5c8e6daf1c47cbc5c157606d90ba694959
Robert Helmer <rhelmer@rhelmer.org>
2016-03-09 09:07:22 -0800

    refactor control enabling into a function

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const panels = require("sdk/panel");
 const tabs = require("sdk/tabs");
 const self = require("sdk/self");
 const data = require("sdk/self").data;
+const { window: { document } } = require('sdk/addon/window');
+const { getTabContentWindow, getActiveTab } = require('sdk/tabs/utils');
+const { getMostRecentBrowserWindow } = require('sdk/window/utils');
 
 // This add-on uses built-in tracking protection, which doesn't have
 // hooks for add-ons to use yet.
 const {Cu} = require("chrome");
 Cu.import("resource://gre/modules/Services.jsm");
 
-// enable tracking protection globally
+// Enable tracking protection globally when add-on is installed.
 Services.prefs.setBoolPref("privacy.trackingprotection.enabled", true);
 
 let button = ToggleButton({
@@ -51,19 +54,7 @@ function normalizeUrl(url) {
   return Services.io.newURI(hostPort, null, null);
 }
 
-// a new tab has been selected
-tabs.on("activate", () => {
-  console.log("active:", tabs.activeTab.url);
-});
-
-// DOM has loaded but page isn't finished, reset the add-on controls
-tabs.on("ready", (tab) => {
-  panel.port.emit("reset");
-});
-
-// page is loaded from back/forward cache
-// This also fires when regular pages are done loading.
-tabs.on("pageshow", (tab) => {
+function enableControls() {
   let normalizedUrl = normalizeUrl(tabs.activeTab.url);
   if (Services.perms.testPermission(normalizedUrl, "trackingprotection")) {
     console.log("tracking protection *not* already active for:", normalizedUrl.spec);
@@ -72,6 +63,23 @@ tabs.on("pageshow", (tab) => {
     console.log("tracking protection already active for:", normalizedUrl.spec);
     panel.port.emit("enabled");
   }
+}
+
+// a new tab has been selected, reset the add-on controls
+tabs.on("activate", () => {
+  panel.port.emit("reset");
+  enableControls();
+});
+
+// DOM has loaded but page isn't finished, reset the add-on controls
+tabs.on("ready", (tab) => {
+  panel.port.emit("reset");
+});
+
+// Page is loaded from back/forward cache.
+// This also fires when regular pages are done loading.
+tabs.on("pageshow", (tab) => {
+  enableControls();
 });
 
 panel.port.on("toggle", (addonMessage) => {
